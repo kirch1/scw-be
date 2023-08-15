f683df6aac7d0531f4277a7217f22c58f57ddf95
Jason Kirchman <kirchman.jason@icloud.com>
2023-08-15 08:50:40 -0500

    Set up endpoint for getting all sets or sets for a year

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"scw-be/pkg/db/models"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,6 +18,10 @@ func StartAPI(pgdb *pg.DB) *chi.Mux {
 	//add middleware, in this case store DB to use later
 	r.Use(middleware.Logger, middleware.WithValue("DB", pgdb))
 
+	r.Route("/sets/{year}", func(r chi.Router) {
+		r.Get("/", getSets)
+	})
+
 	r.Route("/sets", func(r chi.Router) {
 		r.Get("/", getSets)
 	})
@@ -26,6 +33,58 @@ func StartAPI(pgdb *pg.DB) *chi.Mux {
 	return r
 }
 
+type SetsResponse struct {
+	Success bool          `json:"success"`
+	Error   string        `json:"error"`
+	Sets    []*models.Set `json:"sets"`
+}
+
 func getSets(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("sets"))
+
+	year := chi.URLParam(r, "year")
+	log.Print(year)
+
+	pgdb, ok := r.Context().Value("DB").(*pg.DB)
+	if !ok {
+		res := &SetsResponse{
+			Success: false,
+			Error:   "Could not get DB from context",
+			Sets:    nil,
+		}
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	sets, err := models.GetSets(pgdb, year)
+	if err != nil {
+		res := &SetsResponse{
+			Success: false,
+			Error:   err.Error(),
+			Sets:    nil,
+		}
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res := &SetsResponse{
+		Success: true,
+		Error:   "",
+		Sets:    sets,
+	}
+
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		log.Printf("error encoding sets: %v\n", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
--- pkg/db/models/set.go
+++ pkg/db/models/set.go
@@ -1,9 +1,25 @@
 package models
 
+import "github.com/go-pg/pg/v10"
+
 type Set struct {
-	ID    int64  `json:"id"`
-	Sport string `json:"sport"`
-	Brand string `json:"brand"`
-	Year  int64  `json:"year"`
-	Set   string `json:"set"`
+	tableName struct{} `pg:"vw_sets"`
+	ID        int64    `json:"id"`
+	Sport     string   `json:"sport"`
+	Brand     string   `json:"brand"`
+	Year      int64    `json:"year"`
+	Set       string   `json:"set"`
+}
+
+func GetSets(db *pg.DB, year string) ([]*Set, error) {
+	sets := make([]*Set, 0)
+	var err error
+
+	if year == "" {
+		err = db.Model(&sets).Select()
+	} else {
+		err = db.Model(&sets).Where("year = ?", year).Select()
+	}
+
+	return sets, err
 }
